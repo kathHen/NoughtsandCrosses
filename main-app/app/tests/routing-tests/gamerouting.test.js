3f7d5089fecd50c1549eb77f1bd485a2c6d1375e
kathHen <kath.henderson@tombola.com>
2015-10-23 16:03:55 +0100

    Redid the gamerouting test - actually tests the stuff now with more templateCache stuff implemented with the testing. IGNORE OTHER PULL - this is whatcha want

--- main-app/app/tests/routing-tests/gamerouting.test.js
+++ main-app/app/tests/routing-tests/gamerouting.test.js
@@ -19,31 +19,43 @@
                 $httpBackend = _$httpBackend_;
                 $templateCache = _$templateCache_;
             });
+            $templateCache.put('html/playerselect.html', 'html/playerselect.html');
+            $templateCache.put('html/gameplay.html', 'html/gameplay.html');
+            $templateCache.put('html/winplayer.html', 'html/winplayer.html');
+            $templateCache.put('html/draw.html', 'html/draw.html');
+
             spyonState = sinon.sandbox.spy(mocks.fakeState, 'go');
             $rootscope.$digest();
 
         });
 
-        it('Make sure the default state is playerselect', function(){
-            $state.current.url.should.equal('/playerselect');
+        it('Make sure the default state is playerselect', function () {
+            var state = $state.get('playerselection');
+            should.exist(state);
+            state.url.should.equal('/playerselect');
+            state.controller.should.equal('MainController');
+            state.templateProvider($templateCache).should.equal($templateCache.get('html/playerselect.html'));
         });
 
-        it('Gameplay should equal gameplay', function () {
-            $state.go('gameplayInProgress');
-            $timeout.flush();
-            $state.current.url.should.equal('/gameplay');
+        it('Gameplay should equal gameplay', function(){
+            var state = $state.get('gameplayInProgress');
+            should.exist(state);
+            state.url.should.equal('/gameplay');
+            state.templateProvider($templateCache).should.equal($templateCache.get('html/gameplay.html'));
         });
 
         it('Gameplay in progress should equal to Win', function () {
-            $state.go('gameplayInProgress.win');
-            $timeout.flush();
-            $state.current.url.should.equal('/win');
+            var state = $state.get('gameplayInProgress.win');
+            should.exist(state);
+            state.url.should.equal('/win');
+            state.templateProvider($templateCache).should.equal($templateCache.get('html/winplayer.html'));
         });
 
-        it('Gameplay in progress should equal to Win', function () {
-            $state.go('gameplayInProgress.draw');
-            $timeout.flush();
-            $state.current.url.should.equal('/draw');
+        it('Gameplay in progress should equal to Draw', function () {
+            var state = $state.get('gameplayInProgress.draw');
+            should.exist(state);
+            state.url.should.equal('/draw');
+            state.templateProvider($templateCache).should.equal($templateCache.get('html/draw.html'));
         });
 
         afterEach(function(){
